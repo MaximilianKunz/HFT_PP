9e05faa60e272fe68650f353d4925df09055c09a
Maria <maria.ponce.guzman@gmail.com>
2021-02-08 17:42:06 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,10 +1,22 @@
 import glob
+import pandas as pd
 import numpy as np
 from typing import Dict
+
+from Project.HFT_PP.import_datasets import nodes
 
 file2dm = glob.glob('*.2dm')
 file = open(file2dm[0], 'r')
 print(file2dm)
+
+filehdft = glob.glob('*.dat')
+print(filehdft)
+#path = str(path)
+#if filehdft.endswith('*AL.dat'):
+#    path = extractall (outpatch)
+#else:
+#    raise ValueError('Unsupported archive provided. Method supports only .zip/.gz files.')
+file2 = open('Datasets_AL.dat')
 
 class read2DM:
 
@@ -39,4 +51,36 @@ class read2DM:
         z = list4[:, 3]
         print(list4)
 
-m = read2DM.list_nodes(file2dm)
+    def num_fractions(self):
+        fractions = []
+        for line in iter(file):
+            if line.startswith('GP_VAL 2 28'):
+                fractions.append(line.split())
+        fractions = np.asanyarray(fractions)
+        fractions2 = np.delete (fractions, 0, 1)
+        fractions3 = np.asanyarray(fractions2).astype(float)
+        numfractions = fractions3 [:, 2]
+        print (numfractions)
+
+class readSedResults:
+
+    def num_tstep(self) -> None:
+        timestep = 0
+        for line in iter(file2):
+            if line.startswith('TS'):
+                timestep += 1
+        timestep2= timestep / 10
+        print("Number of time steps: {0}".format(timestep2))
+
+    def list_tstep(self):
+        tstep = []
+        for line in iter(file2):
+            if line.startswith('TS'):
+                tstep.append(line.split())
+        tstep = np.asanyarray(tstep)
+        tstep2 = np.delete(tstep, 0, 1)
+        tstep3 = np.asanyarray(tstep2).astype(float)
+        tstep4 = tstep3[0:9:1, 1]
+        print (tstep4)
+
+m = readSedResults.num_tstep(filehdft)
